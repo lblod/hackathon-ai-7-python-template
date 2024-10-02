1fec338e0f9b8693d97b8eae0e7d40024d6d38b0
abeforgit <arnebertrand@gmail.com>
2024-10-02 13:34:53 +0200

    set higher timeout

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ if [ ${MODE:-""} == "development" ]; then
     if [ -f /app/requirements.txt ]; then pip install -r /app/requirements.txt; fi
     exec flask --app web.py run --debug --port 80 --host "0.0.0.0"
 else 
-    exec gunicorn 'web:app' -b '0.0.0.0:80' --pythonpath "/usr/src/app:/app" 
+    exec gunicorn 'web:app' -b '0.0.0.0:80' --pythonpath "/usr/src/app:/app" --timeout 100
 fi
